b22fbbcbe201454b26acf30a45a3bea63f45ad98
kotsaf <safqwer.136@gmail.com>
2024-10-10 16:57:41 +0300

    Add files via upload

---
+++ module_8.2.py
@@ -0,0 +1,47 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
+    return (result, incorrect_data)
+
+def calculate_average(numbers):
+    try:
+        sum_ = personal_sum(numbers)
+        avg = sum_[0] / (len(numbers)  - sum_[1])
+        return avg
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print(f'В numbers записан некорректный тип данных')
+        return None
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
+
+
+
+
+
+
+
+# Некорректный тип данных для подсчёта суммы - 1
+# Некорректный тип данных для подсчёта суммы - ,
+# Некорректный тип данных для подсчёта суммы -
+# Некорректный тип данных для подсчёта суммы - 2
+# Некорректный тип данных для подсчёта суммы - ,
+# Некорректный тип данных для подсчёта суммы -
+# Некорректный тип данных для подсчёта суммы - 3
+# Результат 1: 0
+# Некорректный тип данных для подсчёта суммы - Строка
+# Некорректный тип данных для подсчёта суммы - Ещё Строка
+# Результат 2: 2.0
+# В numbers записан некорректный тип данных
+# Результат 3: None
+# Результат 4: 26.5
